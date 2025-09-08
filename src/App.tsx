56a8174045255ea528e00a42d1630a73ba9373f5
nyeonseok <aragorn2358@naver.com>
2025-09-08 14:31:36 +0900

    feat: App.tsx작성

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,30 @@ import { useState } from "react";
 import Modal from "./components/Modal";
 
 function App() {
-  const [open, setOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalText, setModalText] = useState("");
+  const [modalTitle, setModalTitle] = useState("");
+  const [modalOnConfirm, setModalOnConfirm] = useState<(() => void) | null>(
+    null
+  );
+  const modalOpen = () => {
+    setModalTitle("모달 Title");
+    setModalText("모달 Text");
+    setModalOnConfirm(() => () => {
+      //넣고 싶은 함수 로직 넣으면 됨, 넣기 싫으면 setModalOnConfirm을 안 적어도 괜찮다.(그럼 그냥 확인 누를 시 모달창 닫힘)
+    });
+    setIsOpen(true);
+  };
 
   return (
     <div>
-      <button onClick={() => setOpen(true)}>Open Modal</button>
-      {open && (
+      <button onClick={() => modalOpen()}>Open Modal</button>
+      {isOpen && (
         <Modal
-          setIsOpen={setOpen}
-          title="테스트 모달"
-          text="모달 내용 확인"
-          onConfirm={() => alert("확인 클릭됨")}
+          setIsOpen={setIsOpen}
+          text={modalText}
+          title={modalTitle}
+          onConfirm={modalOnConfirm || undefined}
         />
       )}
     </div>
