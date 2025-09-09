285f07f7996f69bedbf2ec1e2e2a708e3b7ab280
nyeonseok <aragorn2358@naver.com>
2025-09-09 10:24:43 +0900

    chore: .gitignore에 node_modules, dist 추가

--- dist/components/Modal.d.ts
+++
@@ -1,9 +0,0 @@
-import "../css/modal.css";
-type Props = {
-    setIsOpen: (isOpen: boolean) => void;
-    text: string;
-    title: string;
-    onConfirm?: () => void;
-};
-export default function Modal({ setIsOpen, text, title, onConfirm }: Props): import("react/jsx-runtime").JSX.Element;
-export {};
